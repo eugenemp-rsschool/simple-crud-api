661665b018a7bd9e3a7965752894bedd1e1e9239
eugenemp <phoenixlp91@mail.ru>
2021-11-28 23:56:13 +0300

    feat: implement person object validation

---
+++ src/modules/validate-person.js
@@ -0,0 +1,24 @@
+const validatePerson = (person) => {
+  const obj = JSON.parse(person);
+
+  if (!('name' in obj) || !('age' in obj) || !('hobbies' in obj)) return false;
+
+  if (
+    typeof obj.name !== 'string' ||
+    typeof obj.age !== 'number' ||
+    !Array.isArray(obj.hobbies)
+  )
+    return false;
+
+  if (
+    obj.hobbies.length > 0 &&
+    obj.hobbies.every((el) => typeof el === 'string')
+  )
+    return false;
+
+  return true;
+};
+
+module.exports = {
+  validatePerson,
+};
